ce724ab53ec6ff7fe4c79ec8715a9c7c7df702a1
Admin <Admin@DESKTOP-414SEUC>
2024-11-15 13:33:06 +0530

    Added GitHub class

---
+++ Automation/src/github/GitHub.java
@@ -0,0 +1,12 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHub {
+@Test
+public void addMessage()
+{
+	Reporter.log("Welcome to GitHub",true);
+}
+}
